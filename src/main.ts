bfba04f6cbd8be0a45c709eb35ebe9f00d6f89bc
Lang Minh Nguyên <3langn@gmail.com>
2023-03-16 23:55:11 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,34 +6,29 @@ import {
 } from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
 import helmet from "helmet";
-import * as morgan from "morgan";
 import rateLimit from "express-rate-limit";
 import * as compression from "compression";
 
-import {
-  initializeTransactionalContext,
-  patchTypeORMRepositoryWithBaseRepository,
-} from "typeorm-transactional-cls-hooked";
 import { AppModule } from "./app.module";
 import { SharedModule } from "./shared/services/shared.module";
 import { ApiConfigService } from "./shared/services/api-config.service";
 import { HTTPLogger } from "./common/interceptor/logger";
-import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
   // initializeTransactionalContext();
   // patchTypeORMRepositoryWithBaseRepository();
 
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   app.setGlobalPrefix("/api");
-  // app.use(
-  //   rateLimit({
-  //     windowMs: 15 * 60 * 1000,
-  //     max: 100,
-  //   }),
-  // );
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 10000,
+    }),
+  );
+
   app.use(compression());
 
   app.useGlobalInterceptors(
